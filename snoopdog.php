0a25c5968f76f2281e81600f6731337e07f86a69
dmytri <dk@trick.ca>
2012-03-30 18:22:51 +0200

    added header

--- snoopdog.php
+++ snoopdog.php
@@ -1,4 +1,14 @@
 <?php
+/*
+  snoopDog IRCBot Microframework
+  Dmytri Kleiner <dk@trick.ca>
+
+  This program is free software. 
+  It comes without any warranty, to the extent permitted by
+  applicable law. You can redistribute it and/or modify it under the
+  terms of the Do What The Fuck You Want To Public License v2.
+  See http://sam.zoy.org/wtfpl/COPYING for more details. 
+*/
 
 Class SnoopDog {
 
